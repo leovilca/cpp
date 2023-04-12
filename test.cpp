c07a5bc22bcd35d6dd3653272ac435f55edf23ca
Leonardo Vilca <leovilca63@gmail.com>
2023-04-12 05:02:31 -0300

    Basic

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define jump cout<<endl
+#define forn(a,b,c) for(int a=b;a<c;a++)
+#define show(a) for(auto i:a) cout<<i<<space;cout<<endl
+#define input(a) for(auto& i:a) cin>>i
+#define all(a) a.begin(),a.end();
+int main(){
+    //ifstream cin("test.txt");
+    
+    //System(pause);
+    //cin.close()
+    return 0;
+}
