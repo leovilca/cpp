0a62275a679a86a1b6d90b01f8228dbb3d1fd3d3
Leonardo Vilca <leovilca63@gmail.com>
2023-04-12 16:04:21 -0300

    Actualizacion.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define show(a) cout<<a<<endl;
 #define showAll(a) for(auto i:a) cout<<i<<space;cout<<endl
 #define input(a) for(auto& i:a) cin>>i
-#define all(a) a.begin(),a.end();
+#define all(a) a.begin(),a.end()
 int main(){
     //ifstream cin("test.txt");
     
