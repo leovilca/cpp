c83f11734e0768acf79ab3cf6a932ebd8f1d70c0
Leonardo Vilca <leovilca63@gmail.com>
2023-04-12 16:11:49 -0300

    Listo.

--- test.cpp
+++ test.cpp
@@ -3,12 +3,12 @@ using namespace std;
 #define ll long long
 #define forn(a,b,c) for(ll a=b;a<c;a++)
 #define show(a) cout<<a<<endl;
-#define showAll(a) for(auto i:a) cout<<i<<space;cout<<endl
+#define showAll(a) for(auto i:a) cout<<i<<" ";cout<<endl
 #define input(a) for(auto& i:a) cin>>i
 #define all(a) a.begin(),a.end()
 int main(){
     //ifstream cin("test.txt");
-    
+
     //System(pause);
     //cin.close()
     return 0;
