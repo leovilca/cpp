6bc207147bf46d953aa44aabd4b05d2b50a9273a
Leonardo Vilca <leovilca63@gmail.com>
2023-04-12 05:28:52 -0300

    Basic

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define jump cout<<endl
-#define forn(a,b,c) for(int a=b;a<c;a++)
-#define show(a) for(auto i:a) cout<<i<<space;cout<<endl
+#define forn(a,b,c) for(ll a=b;a<c;a++)
+#define show(a) cout<<a<<endl;
+#define showAll(a) for(auto i:a) cout<<i<<space;cout<<endl
 #define input(a) for(auto& i:a) cin>>i
 #define all(a) a.begin(),a.end();
 int main(){
-	//ifstream cin("input.txt");
-	
-	//System(pause);
-	//cin.close()
-	return 0;
+    //ifstream cin("test.txt");
+    
+    //System(pause);
+    //cin.close()
+    return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define jump cout<<endl
-#define forn(a,b,c) for(int a=b;a<c;a++)
-#define show(a) for(auto i:a) cout<<i<<space;cout<<endl
+#define forn(a,b,c) for(ll a=b;a<c;a++)
+#define show(a) cout<<a<<endl;
+#define showAll(a) for(auto i:a) cout<<i<<space;cout<<endl
 #define input(a) for(auto& i:a) cin>>i
 #define all(a) a.begin(),a.end();
 int main(){
