b70ec485478d17f209250474d4792344a50e6dd5
Leonardo Vilca <leovilca63@gmail.com>
2023-04-14 06:26:07 -0300

    Agregando macros.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 #define ll long long
+#define pb push_back
 #define forn(a,b,c) for(int a=b;a<c;a++)
 #define show(a) cout<<a<<"\n"
 #define showAll(a) for(auto i:a) cout<<i<<" ";cout<<"\n"
@@ -9,7 +10,7 @@ using namespace std;
 #define all(a) a.begin(),a.end()
 #define DBG(a) cout<<#a<<" = "<<a<<"\n"
 #define RAYA cout<<==============<<"\n"
-int main(){
+int main(){FIN;
     //ifstream cin("test.txt");
     
     //System("pause");
