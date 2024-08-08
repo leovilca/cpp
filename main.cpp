6766383921e6c7a5d954c17ea63fdeb7ac862796
leovilca <65786858+leovilca@users.noreply.github.com>
2024-08-08 16:25:35 -0300

    Actualizando Macros

--- main.cpp
+++ main.cpp
@@ -1,23 +1,43 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h>
 #define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define ll long long
-#define f first
-#define s second
 #define pb push_back
-#define mp make_pair
-#define forn(a,b,c) for(int a=b;a<c;a++)
+#define fore(a,b,c) for(int a=b; a<c; ++a)
+#define dfore(a,b,c) for(int a=b; a>=c; --a)
+#define SZ(a) ((int)a.size())
+#define fst first
+#define snd second
 #define show(a) cout<<a<<"\n"
 #define showAll(a) for(auto i:a) cout<<i<<" ";cout<<"\n"
 #define input(a) for(auto& i:a) cin>>i
 #define all(a) a.begin(),a.end()
-#define DBG(a) cout<<#a<<" = "<<a<<"\n"
-#define RAYA cout<<==============<<"\n"
+#define DGB(a) cout<<#a<<" = "<<a<<"\n"
+#define RAYA cout<<"=============="<<"\n"
+#define jump cout<<"\n"
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define MAXN 200005
+#define ALPH 26
+#define M 1000000007
+#define MAXINT (1<<30)
+#define MAXll (1ll<<60)
+#define PI 3.141592653
+using namespace std;
+typedef long long ll;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+
+//void solve()
+
 int main(){
     FIN;
-    //ifstream cin("input.txt");
+    //ifstream cin("input.in");
     //ofstream cout("output.txt");
-
+    ll t;
+    //t = 1;
+    cin>>t;
+    while(t--){ // solve()
+        
+    }
     //system("pause");
     //cin.close();
     //cout.close();
